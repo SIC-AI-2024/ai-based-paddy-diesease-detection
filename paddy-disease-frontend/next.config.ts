9e69f0c3b41558a99cd56ea69d9c0e7a78b460c8
itmaybehimm <himansupradhan472@gmail.com>
2024-11-17 10:09:21 +0545

    kaggle notbook for resnet50

--- paddy-disease-frontend/next.config.ts
+++ paddy-disease-frontend/next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
-import { withHydrationOverlay } from "@builder.io/react-hydration-overlay/next";
+
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withHydrationOverlay({
-  appRootSelector: "main",
-})(nextConfig);
+export default nextConfig
